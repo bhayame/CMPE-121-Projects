96814a3110186d63e5c0d17fe1ca69743f4302fd
Brandon Hayame <bhayame@ucsc.edu>
2019-10-17 00:07:16 -0700

    Upload New File

---
+++ labs/lab1/design/1a - Control Register Blink/main.c
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        Control_Reg_1_Write(1);
+        CyDelay(1000);
+        Control_Reg_1_Write(0);
+        CyDelay(1000);
+    }
+}
+
+/* [] END OF FILE */
