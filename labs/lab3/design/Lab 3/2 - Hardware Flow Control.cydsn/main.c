000ef5e1b85d0ad4d38b39f4e041ab60112327c4
Brandon Hayame <bhayame@ucsc.edu>
2019-10-30 14:47:39 -0700

    Changed Rx/Tx connection to internal

--- labs/lab3/design/Lab 3/2 - Hardware Flow Control.cydsn/main.c
+++ labs/lab3/design/Lab 3/2 - Hardware Flow Control.cydsn/main.c
@@ -2,28 +2,28 @@
 #define BLOCK_SIZE 4096
 
 static uint8 transmitArray[BLOCK_SIZE - 1], receiveArray[BLOCK_SIZE - 1] = {0};
-static uint16 transmitCount = 0, receiveCount = 0, errorFlag = 0, finishedFlag = 0;
+static uint16 transmitCount = 0, receiveCount = 0, errorCount = 0, finishedFlag = 0;
 
 CY_ISR(txISR){
     uint8 txStatus = UART_ReadTxStatus();
+    if(transmitCount == BLOCK_SIZE){
+            CyDelay(1);
+            finishedFlag = 1;
+    }
     if(txStatus && UART_TX_STS_FIFO_NOT_FULL == UART_TX_STS_FIFO_NOT_FULL){
         UART_PutChar(transmitArray[transmitCount]);
         transmitCount++;      
     }
-    if(transmitCount == BLOCK_SIZE){
-            CyDelay(1);
-            finishedFlag = 1;
-        }
 }
 
 CY_ISR(timerISR){
     uint8 rxStatus = UART_ReadRxStatus();
-    while(UART_ReadRxStatus() && UART_RX_STS_FIFO_NOTEMPTY == UART_RX_STS_FIFO_NOTEMPTY){
-        receiveArray[receiveCount] = UART_GetChar();
-        receiveCount++;
-    }
     if(rxStatus && UART_RX_STS_OVERRUN == UART_RX_STS_OVERRUN){
-        errorFlag++;
+        errorCount++;
+    }
+    while(UART_ReadRxStatus() && UART_RX_STS_FIFO_NOTEMPTY == UART_RX_STS_FIFO_NOTEMPTY){
+        receiveArray[receiveCount] = UART_ReadRxData();
+        receiveCount++;
     }
 }
 
@@ -53,7 +53,7 @@ int main(void)
             LCD_Char_PrintNumber(mismatchCount);
             LCD_Char_Position(1,0);
             LCD_Char_PrintString("ERRORS:");
-            LCD_Char_PrintNumber(errorFlag);
+            LCD_Char_PrintNumber(errorCount);
             finishedFlag = 0;
         }
     }
