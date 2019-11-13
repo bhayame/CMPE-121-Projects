240c422233ff9139a0cd7d2a3044d80e37840a38
Brandon Tyler Hayame <bhayame@crm.ucsc.edu>
2019-11-13 15:47:19 -0800

    Added finished Part 1 PSoC files

--- labs/lab5/PSoC/design/Lab 5/Part 1 - Remote Control of LED brightness with PWM.cydsn/main.c
+++ labs/lab5/PSoC/design/Lab 5/Part 1 - Remote Control of LED brightness with PWM.cydsn/main.c
@@ -26,9 +26,7 @@ int main(void)
     USBFS_Start(USBFS_DEVICE, USBFS_5V_OPERATION);
 
     /* Wait until device is enumerated by host. */
-    while (USBFS_GetConfiguration() == 0u)
-    {
-    }
+    while (!USBFS_GetConfiguration()){};
 
     /* Enable OUT endpoint to receive data from host. */
     USBFS_EnableOutEP(OUT_EP_NUM);
@@ -36,17 +34,19 @@ int main(void)
     for(;;)
     {
         /* Check if configuration is changed. */
-        if (USBFS_IsConfigurationChanged() != 0u)
+        if (USBFS_IsConfigurationChanged())
         {
             /* Re-enable endpoint when device is configured. */
-            if (USBFS_GetConfiguration() != 0u)
+            if (USBFS_GetConfiguration())
             {
                 /* Enable OUT endpoint to receive data from host. */
                 USBFS_EnableOutEP(OUT_EP_NUM);
             }
         }
+        
+        while (USBFS_GetEPState(0x1) != USBFS_IN_BUFFER_EMPTY){};
         /*Load ADC value into IN and send to host*/
-        USBFS_LoadInEP(IN_EP_NUM, ADC_Read16(), BLOCK_SIZE);     
-        CyDelay(1);
+        uint8 adcValue = ADC_Read16();
+        USBFS_LoadInEP(IN_EP_NUM, &adcValue, BLOCK_SIZE);
     }
 }
