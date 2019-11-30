87ee8e575c5ed367a21b1d8a2f3acd2648af0d6e
Brandon Tyler Hayame <bhayame@ucsc.edu>
2019-11-29 18:12:57 -0800

    Started function in data.c to fill array with adc values

--- project/raspi/oscilloscope/src/data.c
+++ project/raspi/oscilloscope/src/data.c
@@ -1,10 +1,16 @@
 #include "scope.h"
 
-int triggerDetect(int* data, char* triggerSlope, int triggerLevel){
+
+
+int triggerSweep(int* data, char* triggerSlope, int triggerLevel){
 	if(strcmp(triggerSlope, "pos") == 0){
 	
 	}
 	if(strcmp(triggerSlope, "neg") == 0){
 		
 	}
+}
+
+int freeSweep(int* data, int nSamples){
+	
 }
