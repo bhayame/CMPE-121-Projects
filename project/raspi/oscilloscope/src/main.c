8fc0e18ece4087dca01c4bd5c6b07c0a18b50cb8
Brandon Tyler Hayame <bhayame@ucsc.edu>
2019-11-29 18:13:18 -0800

    Laid out main template for main.c

--- project/raspi/oscilloscope/src/main.c
+++ project/raspi/oscilloscope/src/main.c
@@ -6,19 +6,83 @@
  * .c files included.
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <wiringPiI2C.h>
 
 #include "scope.h"
 
+#define  I2C_SLAVE_ADDR 0x53 // 7-bit I2C address of PSoC
+
 int main(int argc, char* argv[]){
-	scopeParameters userParameters;		//Create struct to store user scope parameters from cmdargs
-	if (cmdParse(&userParameters, argc, argv) != 0){		//Populate userParameters with arguments and check for success
+	int width, height; // Width and height of screen in pixels
+	int margin = 10; // Margin spacing around screen
+	int xdiv = 10; // Number of x-axis divisions
+	int ydiv = 8; // Number of y-axis divisions
+	
+	int samples_per_screen;	//Number of samples neeeded to populate screen
+	
+	int channel1_data[10000]; // Data samples from Channel 1
+	int channel2_data[10000]; // Data samples from Channel 2
+
+	data_point channel1_points[10000];	//Channel 1 proccessed data
+	data_point channel2_points[10000];	//Channel 2 proccessed data
+	
+	VGfloat wave1color[4] = {240, 0, 0, 0.5}; // Color for displaying Channel 1 data
+    VGfloat wave2color[4] = {200, 200, 0, 0.5}; // Color for displaying Channel 2 data
+	
+	scopeParameters userParameters;		//Struct to store user scope parameters from cmdargs
+	
+	if (cmdParse(&userParameters, argc, argv) != 0){	//Populate userParameters with arguments and check for success
 		return 1;
 	}
 	
-	for(;;){
+	int fd;	//I2C variables
+    unsigned int pot1_data, pot2_data;
 	
+	// Initialize the I2C interface.
+    // It returns a standard file descriptor.
+    fd = wiringPiI2CSetup(I2C_SLAVE_ADDR);
+    if (fd == -1){
+		perror("I2C device not found\n");
+    }
+	
+	samples_per_screen = (userParameters.xscale * userParameters.sampleRate * xdiv / 1000);		//Calculate # of samples needed to populate screen
+	
+	saveterm(); // Save current screen
+	init(&width, &height); // Initialize display and get width and height
+	Start(width, height);
+	rawterm(); // Needed to receive control characters from keyboard, such as ESC
+	
+	/* SET UP USB */
+	
+	for(;;){
+		if(strcmp(userParameters.mode, "free") ==0){
+			/* COLLECT  samples_per_screen SAMPLES INTO channel1_data FREELY */
+			/* COLLECT  samples_per_screen SAMPLES INTO channel2_data FREELY */
+		}
+		if(strcmp(userParameters.mode, "trigger") ==0){
+			/* COLLECT  samples_per_screen SAMPLES INTO channel1_data USING TRIGGER */
+			/* COLLECT  samples_per_screen SAMPLES INTO channel2_data USING TRIGGER */
+		}
+		
+		WindowClear();
+		Start(width, height);
+		
+		drawBackground(width, height, xdiv, ydiv, margin);
+		printScaleSettings(xscale, yscale, width-300, height-50, textcolor);
+		
+		pot1_data = wiringPiI2CRead(fd);
+		pot2_data = wiringPiI2CRead(fd);
+    
+		processSamples(channel1_data, samples_per_screen, 0, width, userParameters.yscale, channel1_points);
+		processSamples(channel2_data, samples_per_screen, 0, width, userParameters.yscale, channel2_points);
+		plotWave(channel1_points, samples_per_screen, pot1_data, wave1color);		
+		plotWave(channel2_points, samples_per_screen, pot2_data, wave2color);
 	}
+	/*   libusb_close(dev); */
 }
